e4e7a269108b41c362b80b78665d7e466f88f048
zenith-of-the-wired <emmanevascolulw@gmail.com>
2026-05-05 17:58:32 +0800

    DECLARATIONS

---
+++ az-record-sys.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+enum POSITION{
+    DRIVER,
+    REFILLER
+};
+
+enum SEX{
+    MALE,
+    FEMALE
+};
+
+enum MONTHS{
+    JANUARY,
+    FEBRUARY,
+    MARCH,
+    APRIL,
+    MAY,
+    JUNE,
+    JULY,
+    AUGUST,
+    SEPTEMBER,
+    OCTOBER,
+    NOVEMBER,
+    DECEMBER
+};
+
+enum PAYMENT_STATUS{
+    PAID,
+    UNPAID
+};
+
+struct Product{
+    int tankStorage;
+    int year;
+    int day;
+    MONTHS month;
+    int distributedGallons;
+    double priceUnpaid;
+    int waterBottleBatchQty;
+    int sales;
+};
+
+struct Customer{
+    string fullName;
+    string address;
+    int phoneNumber;
+    int gallonsOwnedQty;
+    int day;
+    double priceToPay;
+    PAYMENT_STATUS paymentStatus;
+};
+
+struct Employee{
+    string fullName;
+    string address;
+    int phoneNumber;
+    POSITION position;
+    SEX sex;
+};
+
+void MainMenu();
+
+void ProductManagement()
+{
+
+}
+
+void AddProductInfo()
+{
+
+}
+
+void CustomerManagement()
+{
+
+}
+
+void AddCustomerInfo()
+{
+
+}
+
+void EmployeeManagement()
+{
+
+}
+
+void AddEmployeeInfo()
+{
+
+}
+
+void MainMenu()
+{
+    
+}
+
+int main()
+{
+    
+}
